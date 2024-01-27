602952510ad3d5a14d16b2db87697a6e6af63f1c
devansh <devanshkalra29@gmail.com>
2024-01-27 16:35:39 -0600

    Initial commit

---
+++ src/main/java/com/poc/ticketingplatform/controller/TicketRetrievalController.java
@@ -0,0 +1,15 @@
+package com.poc.ticketingplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicketRetrievalController {
+
+    @GetMapping("/getTicket")
+    public String getTicket() {
+        return "Hello world";
+    }
+
+}
