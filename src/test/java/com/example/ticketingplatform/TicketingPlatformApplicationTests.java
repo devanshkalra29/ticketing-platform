a65c183a43676b17ef5e940b087b663af8474d4a
devansh <devanshkalra29@gmail.com>
2024-01-29 14:10:09 -0600

    changing gradle version

--- src/test/java/com/example/ticketingplatform/TicketingPlatformApplicationTests.java
+++ src/test/java/com/example/ticketingplatform/TicketingPlatformApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.ticketingplatform;
 
+import com.poc.ticketingplatform.TicketingPlatformApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TicketingPlatformApplication.class)
 class TicketingPlatformApplicationTests {
 
 	@Test
